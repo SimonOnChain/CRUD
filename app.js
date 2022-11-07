5f6cfbbc1e7d3be346d48023b6ec5aa64d94d09f
Simon Jokanic <simon05@tutamail.com>
2022-11-07 02:10:24 +0100

    Set the basics

---
+++ app.js
@@ -0,0 +1,23 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+  console.log("button clicked")
+
+    Validation();
+})
+
+let Validation = () => {
+    if (input.value === ""){
+         console.log("failure")
+         msg.innerHTML = "Cannot be blank"
+        
+    }else {
+        console.log("sucesses")
+        msg.innerHTML = "";
+    }
+};
+
